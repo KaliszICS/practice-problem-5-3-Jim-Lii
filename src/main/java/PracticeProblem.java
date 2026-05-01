04d3f0b600938ecbb2b8db9622d3eb7b1313c97d
Jim-Lii <343347795@tdsb.ca>
2026-05-01 17:40:34 +0000

    jkbasdkjbhsdkfsd

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,47 @@
+/*
+File Name: Add/remove
+Author: Jim Li
+Date Created: May 1, 2026
+Date Last Modified: May 1, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+	}
+
+	public static String[] addString(String[] array, String str, int index){
+		String[] array2 = new String[array.length + 1];
+		int j = 0;
+
+    	for (int i = 0; i < array2.length; i++) {
+        	if (i == index) {
+        	    array2[i] = str;
+        	} else {
+        	    array2[i] = array[j];
+        	    j++;
+        	}
+   		}
+		return array2;
+	}
+
+	public static char[] removeChars(char[] array, char target){
+		int counter = 0;
+		for (int i = 0; i < array.length; i++){
+			if (array[i] == target){
+				counter++;
+			}
+		}
+		char[] array2 = new char[array.length - counter];
+		int j = 0;
+		for (int i = 0; i < array.length; i++){
+			if (target != array[i]){
+				array2[j] = array[i];
+				j++;
+			}
+		}
+		return array2;
 	}
 
 }
